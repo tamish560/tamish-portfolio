849e0c592296f64ce092722cddd1dec16a8f7262
Tamish Mhatre <tamishmhatre123@gmail.com>
2026-07-14 01:18:21 +0530

    enable static export for github pages

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   poweredByHeader: false,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
