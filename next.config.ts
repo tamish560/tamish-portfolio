b5beb71f1746c052188fa89b6e6bfdb685d8d7dc
Tamish Mhatre <tamishmhatre123@gmail.com>
2026-07-14 01:18:51 +0530

    add basePath for github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const isGH = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
+  trailingSlash: true,
+  basePath: isGH ? "/tamish-portfolio" : "",
   poweredByHeader: false,
   images: {
     unoptimized: true,
