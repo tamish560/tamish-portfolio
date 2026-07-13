66e98a243a2673198f19f96aafc184ea99f00cea
Tamish Mhatre <tamishmhatre123@gmail.com>
2026-07-14 01:20:59 +0530

    fix robots.ts for static export

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,6 +1,8 @@
 import type { MetadataRoute } from "next";
 import { BASE_URL } from "./constants/data";
 
+export const dynamic = "force-static";
+
 export default function robots(): MetadataRoute.Robots {
   const baseUrl = BASE_URL;
 
