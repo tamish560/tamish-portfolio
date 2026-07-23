1153fe1c6a5c64659057c6df0d06ec35e24514cd
Tamish Mhatre <swift@inbox.bigcat.pw>
2026-07-23 17:39:58 +0000

    fix: always use output:export, basePath from env

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
 import type { NextConfig } from "next";
 
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
-const isExport = process.env.NEXT_PUBLIC_IS_EXPORT === "1";
 
 const nextConfig: NextConfig = {
-  ...(isExport ? { output: "export" } : {}),
+  output: "export",
   ...(basePath ? { basePath } : {}),
   poweredByHeader: false,
   images: {
